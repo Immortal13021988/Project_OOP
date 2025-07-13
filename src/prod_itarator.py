bc2776a7dd48c5c49f9f9f6630820a9ce3cb32d0
iua.suranov <pradedura@mail.ru>
2025-07-13 17:30:58 +0300

    add prod_itarator.py fix products.py

---
+++ src/prod_itarator.py
@@ -0,0 +1,19 @@
+class ProdIterator:
+    """ Итератор"""
+    def __init__(self, category_obj):
+        """Инициализация итератора"""
+        self.category = category_obj
+        self.index = 0
+
+    def __iter__(self):
+        """Получение объекта для итератора"""
+        return self
+
+    def __next__(self):
+        """Переход к следующему значению"""
+        if self.index < len(self.category.products_list):
+            prod = self.category.products_list[self.index]
+            self.index += 1
+            return prod
+        else:
+            raise StopIteration
--- src/products.py
+++ src/products.py
@@ -1,3 +1,6 @@
+from src.prod_itarator import ProdIterator
+
+
 class Product:
     """Класс с информацией о продуктах"""
 
@@ -7,13 +10,23 @@ class Product:
     quantity = int
 
     def __init__(self, name, description, price, quantity=0):
+        """Инициализация класса"""
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        """ Передача строкового значения продукта"""
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        """ Сложение стоимости продуктов на складе """
+        return self.price * self.quantity + other.price * other.quantity
+
     @classmethod
     def new_product(cls, new_dict, prod_list):
+        """ Добавление нового продукта, с проверкой на наличие и стоимости"""
         for prod in prod_list:
             if new_dict["name"] == prod.name:
                 if new_dict["price"] > prod.price:
@@ -41,10 +54,12 @@ class Product:
 
     @property
     def price(self):
+        """ Передача приватного параметра прайс"""
         return self.__price
 
     @price.setter
     def price(self, new_price):
+        """ Обновление цены товара"""
         if new_price > 0:
             if new_price < self.__price:
                 user = input("Подтвердите уменьшение цены: y/n\n")
@@ -66,18 +81,25 @@ class Category:
     category_count = 0
 
     def __init__(self, name, description, products=None):
+        """Инициализация класса"""
         self.name = name
         self.description = description
         self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
+    def __str__(self):
+        """Передача строкового значения категории товаров"""
+        return f"{self.name}, количество продуктов: {sum(item.quantity for item in self.__products)}"
+
     def add_product(self, product: Product):
+        """Добавление нового продукта в категории"""
         self.__products.append(product)
         Category.product_count += 1
 
     @property
     def products_str(self):
+        """Получение строкового значения о товаре в категории"""
         new_string = ""
         for prod in self.__products:
             new_string += (
@@ -87,6 +109,7 @@ class Category:
 
     @property
     def products_list(self):
+        """Получение товаров в категории списком"""
         new_list = []
         for prod in self.__products:
             new_list.append(prod)
@@ -100,38 +123,24 @@ if __name__ == "__main__":  # pragma: no cover
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
+
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3],
     )
 
-    print(category1.products_str)
-    print(category1.product_count)
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products_str)
-    print(category1.product_count)
+    print(str(category1))
 
-    new_product = Product.new_product(
-        {
-            "name": "Samsung Galaxy S23 Ultra",
-            "description": "256GB, Серый цвет, 200MP камера",
-            "price": 180000.0,
-            "quantity": 5,
-        },
-        category1.products_list,
-    )
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
+    print(category1.products)
 
-    new_product.price = 800
-    print(new_product.price)
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
 
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
-    print(category1.products_str)
+    iterator = ProdIterator(category1)
+    for i in iterator:
+        print(i)
